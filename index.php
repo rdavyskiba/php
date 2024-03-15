ceb9c56c28d1644b4278229994877302d553cab2
rdavyskiba <leadmagnit2017@gmail.com>
2024-03-15 18:27:07 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
 echo 'Hello, World!';
 $var = 10;
 echo $result = $var - 4; 
+?>
+</body>
+</html>
